193e37d3517d17d5d492f93676012a67d12fe73e
Jesin Jaice <j99615d@gmail.com>
2024-03-24 23:54:58 +0530

    Adding instant search

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import {
   addToReadingList,
@@ -8,13 +8,15 @@ import {
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
+import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'tmo-book-search',
   templateUrl: './book-search.component.html',
   styleUrls: ['./book-search.component.scss']
 })
-export class BookSearchComponent {
+export class BookSearchComponent implements OnInit,OnDestroy {
 
   booksList$ = this.store.select(getAllBooks);
 
@@ -22,10 +24,24 @@ export class BookSearchComponent {
     term: ''
   });
 
+  unSubscribeSubject$ = new Subject();
   constructor(
     private readonly store: Store,
     private readonly fb: FormBuilder
   ) {}
+
+  ngOnInit(): void {
+    this.searchForm.controls['term'].valueChanges
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        takeUntil(this.unSubscribeSubject$)
+      )
+      .subscribe(
+        () => this.searchBooks(),
+        (err) => console.error(err, 'Something went wrong')
+      );
+  }
 
   get searchTerm(): string {
     return this.searchForm.value.term;
@@ -37,7 +53,6 @@ export class BookSearchComponent {
 
   searchExample() {
     this.searchForm.controls.term.setValue('javascript');
-    this.searchBooks();
   }
 
   searchBooks() {
@@ -46,5 +61,10 @@ export class BookSearchComponent {
     } else {
       this.store.dispatch(clearSearch());
     }
+  }
+
+  ngOnDestroy(): any {
+    this.unSubscribeSubject$.next();
+    this.unSubscribeSubject$.complete();
   }
 }
